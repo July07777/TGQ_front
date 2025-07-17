913cb2201d1e3d3e489078eb595c6fe6d6f1ee45
JJuly <1498677235@qq.com>
2025-07-17 10:26:30 +0800

    数据源页面

--- spark-yun-frontend/src/utils/http/index.ts
+++ spark-yun-frontend/src/utils/http/index.ts
@@ -18,77 +18,76 @@ const message = ElMessage
 const whiteList = ['/vip/auth/open/querySsoAuth']
 
 export const httpOption = {
-  transform: {
-    requestInterceptors: (config: any) => {
-      const authStore = useAuthStore()
-      config.headers['authorization'] = config.headers['authorization'] || authStore.token
-      config.headers['tenant'] = config.headers['tenant'] || authStore.tenantId
+    transform: {
+        requestInterceptors: (config: any) => {
+            const authStore = useAuthStore()
+            config.headers['authorization'] = config.headers['authorization'] || authStore.token
+            config.headers['tenant'] = config.headers['tenant'] || authStore.tenantId
 
-      return config
-    },
-    responseInterceptors: (config: any): any => {
-      // getTokenFromResponse(config);
+            return config
+        },
+        responseInterceptors: (config: any): any => {
+            // getTokenFromResponse(config);
 
-      return config
-    }
-  },
-  requestOptions: {
-    // urlPrefix: 'http://isxcode.com:30211',
-    urlPrefix: import.meta.env.VITE_VUE_APP_BASE_DOMAIN,
-    showSuccessMessage: (msg: string): void => {
-      message.success(msg)
-    },
-    showErrorMessage: (msg: string): void => {
-      message.error(msg)
-    },
-    checkStatus: (status: number, msg: string, showMsg: any, response: any): void => {
-      try {
-        if (status == 401) {
-          router.push({
-            name: 'login'
-          })
-        } else if (status == 403) {
-          message.error('许可证无效，请联系管理员')
-        } else if (status == 404) {
-          if (response.config.url.match('/vip/')) {
-            if (!whiteList.some(url => response.config.url.match(url))) {
-              message.error('请升级到企业版')
-            }
-          } else {
-            showMsg(msg)
-          }
-        } else {
-          showMsg(msg)
+            return config
         }
-      } catch (error) {
-        console.error('error', error)
-        // console.log('err', error);
-        // router.replace({
-        //     name: LOGIN_NAME,
-        //     query: {
-        //         redirect: '/home'
-        //     }
-        // });
-      }
-    }
-  },
-  timeout: 30 * 1e3
+    },
+    requestOptions: {
+        // urlPrefix: 'http://isxcode.com:30211',
+        urlPrefix: 'http://192.168.12.81:8115',
+        // urlPrefix: import.meta.env.VITE_VUE_APP_BASE_DOMAIN,
+        showSuccessMessage: (msg: string): void => {
+            message.success(msg)
+        },
+        showErrorMessage: (msg: string): void => {
+            message.error(msg)
+        },
+        checkStatus: (status: number, msg: string, showMsg: any, response: any): void => {
+            try {
+                if (status == 401) {
+                    router.push({
+                        name: 'login'
+                    })
+                } else if (status == 403) {
+                    message.error('许可证无效，请联系管理员')
+                } else if (status == 404) {
+                    if (response.config.url.match('/vip/')) {
+                        if (!whiteList.some((url) => response.config.url.match(url))) {
+                            message.error('请升级到企业版')
+                        }
+                    } else {
+                        showMsg(msg)
+                    }
+                } else {
+                    showMsg(msg)
+                }
+            } catch (error) {
+                console.error('error', error)
+                // console.log('err', error);
+                // router.replace({
+                //     name: LOGIN_NAME,
+                //     query: {
+                //         redirect: '/home'
+                //     }
+                // });
+            }
+        }
+    },
+    timeout: 30 * 1e3
 }
 
-export const createHttp = (option = {
-}) => {
-  return createAxios(
-    merge(
-      {
-      },
-      {
-        ...httpOption
-      },
-      {
-        ...option
-      }
+export const createHttp = (option = {}) => {
+    return createAxios(
+        merge(
+            {},
+            {
+                ...httpOption
+            },
+            {
+                ...option
+            }
+        )
     )
-  )
 }
 
 export const http = createHttp()
--- spark-yun-frontend/src/views/datasource/index.vue
+++ spark-yun-frontend/src/views/datasource/index.vue
@@ -1,71 +1,57 @@
 <template>
-  <Breadcrumb :bread-crumb-list="breadCrumbList" />
-  <div class="zqy-seach-table">
-    <div class="zqy-table-top">
-      <el-button type="primary" @click="addData">
-        添加数据源
-      </el-button>
-      <div class="zqy-tenant__select">
-        <el-select 
-          v-model="datasourceType" 
-          placeholder="请选择数据源类型" 
-          filterable
-          clearable
-          @change="handleChnage"
-        >
-          <el-option
-            v-for="item in datasourceTypeList"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
-          ></el-option>
-        </el-select>
-      </div>
-      <div class="zqy-seach">
-        <el-input v-model="keyword" placeholder="请输入名称/类型/连接信息/用户名/备注 回车进行搜索" :maxlength="200" clearable
-          @input="inputEvent" @keyup.enter="initData(false)" />
-      </div>
-    </div>
-    <LoadingPage :visible="loading" :network-error="networkError" @loading-refresh="initData(false)">
-      <div class="zqy-table">
-        <BlockTable :table-config="tableConfig" @size-change="handleSizeChange" @current-change="handleCurrentChange">
-          <template #statusTag="scopeSlot">
-            <ZStatusTag :status="scopeSlot.row.status"></ZStatusTag>
-          </template>
-          <template #options="scopeSlot">
-            <div class="btn-group">
-
-              <span v-if="!scopeSlot.row.checkLoading" @click="checkData(scopeSlot.row)">检测</span>
-              <el-icon
-                  v-else
-                  class="is-loading"
-              >
-                <Loading />
-              </el-icon>
-              <el-dropdown trigger="click">
-                <span class="click-show-more">更多</span>
-                <template #dropdown>
-                  <el-dropdown-menu>
-                    <el-dropdown-item @click="showLog(scopeSlot.row)">
-                      日志
-                    </el-dropdown-item>
-                    <el-dropdown-item @click="editData(scopeSlot.row)">
-                      编辑
-                    </el-dropdown-item>
-                    <el-dropdown-item @click="deleteData(scopeSlot.row)">
-                      删除
-                    </el-dropdown-item>
-                  </el-dropdown-menu>
-                </template>
-              </el-dropdown>
+    <Breadcrumb :bread-crumb-list="breadCrumbList" />
+    <div class="zqy-seach-table">
+        <div class="zqy-table-top">
+            <el-button type="primary" @click="addData">添加数据源</el-button>
+            <div class="zqy-tenant__select">
+                <el-select
+                    v-model="datasourceType"
+                    placeholder="请选择数据源类型"
+                    filterable
+                    clearable
+                    @change="handleChnage"
+                >
+                    <el-option
+                        v-for="item in datasourceTypeList"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value"
+                    ></el-option>
+                </el-select>
             </div>
-          </template>
-        </BlockTable>
-      </div>
-    </LoadingPage>
-    <AddModal ref="addModalRef" />
-    <ShowLog ref="showLogRef" />
-  </div>
+            <div class="zqy-seach">
+                <el-input
+                    v-model="keyword"
+                    placeholder="请输入名称/类型/连接信息/用户名/备注 回车进行搜索"
+                    :maxlength="200"
+                    clearable
+                    @input="inputEvent"
+                    @keyup.enter="initData(false)"
+                />
+            </div>
+        </div>
+        <LoadingPage :visible="loading" :network-error="networkError" @loading-refresh="initData(false)">
+            <div class="zqy-table">
+                <BlockTable
+                    :table-config="tableConfig"
+                    @size-change="handleSizeChange"
+                    @current-change="handleCurrentChange"
+                >
+                    <template #statusTag="scopeSlot">
+                        <ZStatusTag :status="scopeSlot.row.status"></ZStatusTag>
+                    </template>
+                    <template #options="scopeSlot">
+                        <div class="btn-group">
+                            <span @click="editData(scopeSlot.row)">编辑</span>
+                            <span @click="deleteData(scopeSlot.row)">删除</span>
+                        </div>
+                    </template>
+                </BlockTable>
+            </div>
+        </LoadingPage>
+        <AddModal ref="addModalRef" />
+        <ShowLog ref="showLogRef" />
+    </div>
 </template>
 
 <script lang="ts" setup>
@@ -77,7 +63,13 @@ import AddModal from './add-modal/index.vue'
 import ShowLog from '../computer-group/computer-pointer/show-log/index.vue'
 
 import { BreadCrumbList, TableConfig, FormData, typeList } from './datasource.config'
-import { GetDatasourceList, AddDatasourceData, UpdateDatasourceData, CheckDatasourceData, DeleteDatasourceData } from '@/services/datasource.service'
+import {
+    GetDatasourceList,
+    AddDatasourceData,
+    UpdateDatasourceData,
+    CheckDatasourceData,
+    DeleteDatasourceData
+} from '@/services/datasource.service'
 import { ElMessage, ElMessageBox } from 'element-plus'
 import { Loading } from '@element-plus/icons-vue'
 
@@ -93,124 +85,123 @@ const breadCrumbList = reactive(BreadCrumbList)
 const tableConfig: any = reactive(TableConfig)
 
 function initData(tableLoading?: boolean) {
-  loading.value = tableLoading ? false : true
-  networkError.value = networkError.value || false
-  GetDatasourceList({
-    page: tableConfig.pagination.currentPage - 1,
-    pageSize: tableConfig.pagination.pageSize,
-    searchKeyWord: keyword.value,
-    datasourceType: datasourceType.value
-  })
-    .then((res: any) => {
-      tableConfig.tableData = res.data.content
-      tableConfig.pagination.total = res.data.totalElements
-      loading.value = false
-      tableConfig.loading = false
-      networkError.value = false
+    loading.value = tableLoading ? false : true
+    networkError.value = networkError.value || false
+    GetDatasourceList({
+        page: tableConfig.pagination.currentPage - 1,
+        pageSize: tableConfig.pagination.pageSize,
+        searchKeyWord: keyword.value,
+        datasourceType: datasourceType.value
     })
-    .catch(() => {
-      tableConfig.tableData = []
-      tableConfig.pagination.total = 0
-      loading.value = false
-      tableConfig.loading = false
-      networkError.value = true
-    })
-}
-
-function addData() {
-  addModalRef.value.showModal((formData: FormData) => {
-    return new Promise((resolve: any, reject: any) => {
-      AddDatasourceData(formData)
         .then((res: any) => {
-          ElMessage.success(res.msg)
-          initData()
-          resolve()
+            console.log(res)
+
+            tableConfig.tableData = res
+            tableConfig.pagination.total = res.length
+            loading.value = false
+            tableConfig.loading = false
+            networkError.value = false
         })
-        .catch((error: any) => {
-          reject(error)
+        .catch(() => {
+            tableConfig.tableData = []
+            tableConfig.pagination.total = 0
+            loading.value = false
+            tableConfig.loading = false
+            networkError.value = true
+        })
+}
+function addData() {
+    addModalRef.value.showModal((formData: FormData) => {
+        return new Promise((resolve: any, reject: any) => {
+            AddDatasourceData(formData)
+                .then((res: any) => {
+                    ElMessage.success(res.msg)
+                    initData()
+                    resolve()
+                })
+                .catch((error: any) => {
+                    reject(error)
+                })
         })
     })
-  })
 }
 
 // 查看日志
 function showLog(e: any) {
-  showLogRef.value.showModal(e.connectLog)
+    showLogRef.value.showModal(e.connectLog)
 }
 
 function editData(data: any) {
-  addModalRef.value.showModal((formData: FormData) => {
-    return new Promise((resolve: any, reject: any) => {
-      UpdateDatasourceData(formData)
-        .then((res: any) => {
-          ElMessage.success(res.msg)
-          initData()
-          resolve()
+    addModalRef.value.showModal((formData: FormData) => {
+        return new Promise((resolve: any, reject: any) => {
+            UpdateDatasourceData(formData)
+                .then((res: any) => {
+                    ElMessage.success(res.msg)
+                    initData()
+                    resolve()
+                })
+                .catch((error: any) => {
+                    reject(error)
+                })
         })
-        .catch((error: any) => {
-          reject(error)
-        })
-    })
-  }, data)
+    }, data)
 }
 
 // 检测
 function checkData(data: any) {
-  data.checkLoading = true
-  CheckDatasourceData({
-    datasourceId: data.id
-  })
-    .then((res: any) => {
-      data.checkLoading = false
-      ElMessage.success(res.msg)
-      initData(true)
+    data.checkLoading = true
+    CheckDatasourceData({
+        datasourceId: data.id
     })
-    .catch(() => {
-      data.checkLoading = false
-    })
+        .then((res: any) => {
+            data.checkLoading = false
+            ElMessage.success(res.msg)
+            initData(true)
+        })
+        .catch(() => {
+            data.checkLoading = false
+        })
 }
 
 // 删除
 function deleteData(data: any) {
-  ElMessageBox.confirm('确定删除该数据源吗？', '警告', {
-    confirmButtonText: '确定',
-    cancelButtonText: '取消',
-    type: 'warning'
-  }).then(() => {
-    DeleteDatasourceData({
-      datasourceId: data.id
+    ElMessageBox.confirm('确定删除该数据源吗？', '警告', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+    }).then(() => {
+        DeleteDatasourceData(data.id)
+            .then((res: any) => {
+                ElMessage.success(res.msg)
+                initData()
+            })
+            .catch(() => {})
     })
-      .then((res: any) => {
-        ElMessage.success(res.msg)
-        initData()
-      })
-      .catch(() => { })
-  })
 }
 
 function inputEvent(e: string) {
-  if (e === '') {
-    initData()
-  }
+    if (e === '') {
+        initData()
+    }
 }
 
 function handleSizeChange(e: number) {
-  tableConfig.pagination.pageSize = e
-  initData(true)
+    tableConfig.pagination.pageSize = e
+    initData(true)
 }
 
 function handleCurrentChange(e: number) {
-  tableConfig.pagination.currentPage = e
-  initData(true)
+    tableConfig.pagination.currentPage = e
+    initData(true)
 }
 
 function handleChnage() {
-  initData()
+    initData()
 }
 
 onMounted(() => {
-  tableConfig.pagination.currentPage = 1
-  tableConfig.pagination.pageSize = 10
-  initData()
+    tableConfig.pagination.currentPage = 1
+    tableConfig.pagination.pageSize = 10
+    initData()
 })
 </script>
